e13b0a442573e8556f9f19636629cc36425dbd78
luiz.amaral <luiz.amaral@aluno.edu.pi.gov.br>
2026-06-10 12:48:53 +0000

    correcAO

--- multiplicacao.test.js
+++ multiplicacao.test.js
@@ -1,10 +1,9 @@
-const multiplicar = require( "./multiplicar");
+const multiplicar = require( "./multiplicacao");
 test("deve multiplicar 6 por 7", () => {
-    expect(multiplicar(6,7)).tobe(42)
+    expect(multiplicar(6,7)).toBe(42)
 });
-test("deve multiplicar dois numeros positivos", () =>{
-expect(multiplicar(6, 7)).toBe(0);
-
+test("deve multiplicar dois numeros positivos", () => {
+ expect(multiplicar(6, 7)).toBe(42);
 });
 test("deve multiplicar um numero negativo com um positivo", () => {
     expect(multiplicar(-6, 7)).toBe(-42);
