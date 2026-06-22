63a1caf5058949f0e5585835c64d968c80e3db6d
luiz.amaral <luiz.amaral@aluno.edu.pi.gov.br>
2026-06-22 12:51:23 +0000

    produtos

--- produto.js
+++ produto.js
@@ -1,7 +1,11 @@
 const produto = {
     nome: "iphone 11 pro",
+    marca: "apple",
     preco: 1500.00,
   };
-  
-  console.log(produto.nome);
-  console.log(produto.preco);
+  for ( let chave in produto ) {
+    console.log(chave, produto[chave]);
+  }
+
+console.log(Object.keys(produto));
+console.log(Object.values(produto));
